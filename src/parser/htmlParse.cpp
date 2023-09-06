d1ae595f04f77686784559b9c82c4aa18e886221
shobhit <shobhit32t@gmail.com>
2023-09-06 08:22:29 +0530

    function to parse the html token table into a parse tree

--- src/parser/htmlParse.cpp
+++ src/parser/htmlParse.cpp
@@ -18,10 +18,7 @@ int htmlParse::checkIsCloseTagComplementary(std::string o_tag, std::string c_tag
     return 1;
 }
 
-/*** REMOVE FOLLOWING COMMENTS ONCE FUNCTION IS FIXED***/
-// this function is not working properly
-// just leaving as it is to get blueprint for later
-// decide about exact rendering detail
+/*** TESTING OF THIS FUNCTION IS PENDING***/
 // returns root of the parsed html DOM
 struct treeNode* htmlParse::htmlParser(std::string fPath) {
     htmlTokenise tokeniser;
@@ -31,21 +28,19 @@ struct treeNode* htmlParse::htmlParser(std::string fPath) {
     while(it != htmlTokenTable.end()) {
         switch (it->tok_attribute) {
             case OPENING_TAG:
-                // create node, set current node pointer to this new node
+                // create node and set current node pointer to this new node
                 createAndSetPtr();
-                it++;
                 break;
             case CLOSING_TAG:
-                // set current tree node pointer to parent node and so on
-                // check if closing node is corresponding close tag of current open node
-                //if(checkIsCloseTagComplementary(it->tok_val, DOM_tree.getCurrentNode()->))
+                // set current tree node pointer to parent node
+                DOM_tree.setCurrentNode(DOM_tree.getCurrentNode()->parentNode);
                 break;
             case INNER_TEXT:
                 // add data
                 DOM_tree.addtxtContent(DOM_tree.getCurrentNode(), it->tok_val);
-                it++;
                 break;
         }
+        // to read next token
         it++;
     }
 }
